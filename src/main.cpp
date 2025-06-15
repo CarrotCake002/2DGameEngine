f2411342ec879ff4c4e4a6de9c76a8b944e59633
CarrotCake <pol.siles-poloepitech.eu>
2025-06-15 19:42:21 +0200

    [ADD] opening of a SFML window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "2D Game Engine");
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
+    return 0;
 }
